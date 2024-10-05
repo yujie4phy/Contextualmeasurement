8c9374a263c0327db57533e1ce64a97fe9c9228e
yujie4phy <119273005+yujie4phy@users.noreply.github.com>
2024-10-05 12:11:45 -0400

    SDPcontextualmeas.py

--- SDPcontextualmeas.py
+++ SDPcontextualmeas.py
@@ -6,7 +6,7 @@
 import Bases
 import Utility
 
-M = Bases.Planor(4)   # different measurements are different in Bases.py
+M = Bases.Planor(4)   # different measurements are given in Bases.py
 Fa, eta = Utility.jrdual(M)    # dual SDP for quantifying white-noise robustness of contextual measurement
 #Fa, eta = Utility.jwdual(M)    # dual SDP for quantifying weight of contextual measurement
 print("Dual variable (joint_M):", Fa)
